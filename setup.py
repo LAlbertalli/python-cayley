eaba18c51383385b0fb43d17b5b5678d42dd14d1
l.albertalli <l.albertalli@gmail.com>
2015-05-18 17:26:05 -0700

    Fixing dependency of setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     name = "cayley",
     version = "0.0.1",
     packages = find_packages(exclude=["tests"]),
-    install_requires = ["requests >= 2.3.0"]
+    install_requires = ["requests >= 2.3.0","six >= 1.4"]
 )
